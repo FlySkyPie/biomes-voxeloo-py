9155805626215bb8f6effdebbba9ea916f201ce1
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2025-10-28 22:25:33 +0800

    fix: Fix entry point

--- src/VoxelooPy/__init__.py
+++ src/VoxelooPy/__init__.py
@@ -1,22 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-from .voxeloo import __doc__, __version__, shards
-
-if TYPE_CHECKING:
-    from . import biomes  # noqa: F401
-    from ._biomes import (
-        VolumeBlock,
-        SparseBlock,
-        BlockList,
-    )
+from .voxeloo import __doc__, __version__, shards, biomes, blocks, culling
 
 __all__ = [
     "__doc__",
     "__version__",
     "shards",
-    "VolumeBlock",
-    "SparseBlock",
-    "BlockList",
+    "biomes",
+    "blocks",
+    "culling",
 ]
