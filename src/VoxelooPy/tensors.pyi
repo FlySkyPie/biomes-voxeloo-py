cdf00ebfcc78062bdea4df86846d570a26ba8fad
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2025-11-01 11:57:58 +0800

    fix: Fix tensors stubs

--- src/VoxelooPy/tensors.pyi
+++ src/VoxelooPy/tensors.pyi
@@ -1,26 +1,54 @@
-from typing import Generic, TypeVar, Tuple, Union, Optional, Any
+from typing import Generic, TypeVar, Tuple
 import numpy as np
-from ctypes import c_uint8, c_uint16, c_uint32, c_uint64, c_int8, c_int16, c_int32, c_int64
+import numpy.typing as npt
+from ctypes import (
+    c_uint8,
+    c_uint16,
+    c_uint32,
+    c_uint64,
+    c_int8,
+    c_int16,
+    c_int32,
+    c_int64,
+)
 
-T = TypeVar('T', bool, int, float)
+T = TypeVar(
+    "T",
+    bool,
+    float,
+    c_uint8,
+    c_uint16,
+    c_uint32,
+    c_uint64,
+    c_int8,
+    c_int16,
+    c_int32,
+    c_int64,
+)
+
+T_Scalar = TypeVar('T_Scalar', bound=np.generic)
 
 class Tensor(Generic[T]):
     def __init__(self, shape: Tuple[int, int, int], fill: T = ...) -> None: ...
     def __str__(self) -> str: ...
     def fill(self, fill: T) -> None: ...
     def get(self, x: int, y: int, z: int) -> T: ...
-    def assign(self, pos: np.ndarray[Any, np.dtype[np.int32]], val: np.ndarray[Any, np.dtype[T]]) -> None: ...
+    def assign(
+        self,
+        pos: npt.NDArray[np.int32],
+        val: npt.NDArray[T_Scalar],
+    ) -> None: ...
     def dumps(self) -> str: ...
     def loads(self, blob: str) -> None: ...
     def dump(self) -> bytes: ...
     def load(self, blob: str) -> None: ...
-    def array(self) -> np.ndarray[Any, np.dtype[T]]: ...
+    def array(self) -> npt.NDArray[T_Scalar]: ...
     @staticmethod
-    def fromarray(array: np.ndarray[Any, np.dtype[T]]) -> 'Tensor[T]': ...
+    def fromarray(array: npt.NDArray[T_Scalar]) -> "Tensor[T]": ...
 
 Tensor_Bool = Tensor[bool]
 Tensor_I8 = Tensor[c_int8]
-Tensor_I16 = Tensor[c_int16] 
+Tensor_I16 = Tensor[c_int16]
 Tensor_I32 = Tensor[c_int32]
 Tensor_I64 = Tensor[c_int64]
 Tensor_U8 = Tensor[c_uint8]
